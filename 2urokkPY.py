07e0c6853039194e84c7635ed3a30a792e0b4a71
combar1 <131698975+combar1@users.noreply.github.com>
2023-09-30 18:40:31 +0300

    Add files via upload
    title переменные, типы данных, функции

---
+++ 2urokkPY.py
@@ -0,0 +1,21 @@
+# # a = 'str'
+# # b = 1
+# # c = True
+# # d = None
+# # e = range
+# # f = 1.5
+# # g = [1,2,3,4,5]
+# # print(type(a))
+# # print(type(b))
+# # print(type(c))
+# # print(type(d))
+# # print(type(e))
+# # print(type(f))
+# # print(type(g))
+# list1 = 'Hello world A sports its in a game'
+str2 = 'a'
+str3 = 'b'
+print(str2 + str3)
+print(len(str2))
+for i in str2:
+    print(i)
---
+++ main.py
@@ -0,0 +1,9 @@
+str = 'jo'
+print(type(str))
+num = 1
+print(type(num))
+bul = True
+print(type(bul))
+num1 = 1.5
+print(type(num1))
+
